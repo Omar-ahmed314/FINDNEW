1869baca9bf86ef6e4a0afaaf94119a657d1420a
Thebrownboy <abdallaxpmostafa@gmail.com>
2021-05-16 23:50:09 +0200

    add indexerDatabase class , this class will implement the indexer database as a map of strings->set of Documents

---
+++ SearchEngine/src/main/java/indexer/IndexerDatabase.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indexer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author thebrownboy
+ */
+public class IndexerDatabase {
+    Map<String,Set<DocumentInfo>> indexerMap ; 
+
+    public IndexerDatabase() {
+        indexerMap=new HashMap<>(); 
+    }
+    
+    
+    public void addDocument(String word, DocumentInfo doc){
+        if(indexerMap.containsKey(word)){
+            Set<DocumentInfo> wordSet= indexerMap.get(word); 
+            wordSet.add(doc);
+            indexerMap.replace(word, wordSet); 
+        }
+        else{
+            Set<DocumentInfo> docSet= new HashSet<>();
+            docSet.add(doc); 
+            indexerMap.put(word, docSet); 
+            
+        }
+        
+    }
+    public Set<DocumentInfo> getWordSet(String word){
+        if(indexerMap.containsKey(word)){
+            return indexerMap.get(word); 
+        }
+        return null ; 
+    }
+    
+    /*TO-DO we should give every Document a specific id or identifer to be able to handle it */
+    
+}
