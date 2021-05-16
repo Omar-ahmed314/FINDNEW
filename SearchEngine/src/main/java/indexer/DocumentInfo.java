009208bf711bf5fa406c478170284e7907e3f366
Thebrownboy <abdallaxpmostafa@gmail.com>
2021-05-16 23:01:39 +0200

    adding the document info class , it will store the info of the document such as URL , TF , ouccrences per tagName

---
+++ SearchEngine/src/main/java/indexer/DocumentInfo.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indexer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author thebrownboy
+ */
+public class DocumentInfo {
+    
+    String URL;
+    int TF ; 
+    Map <String ,Integer>occurences; 
+
+    public DocumentInfo(String URL) {
+        setURL(URL);
+        occurences= new HashMap(); 
+    }
+    
+    
+    public void setURL(String URL){
+        this.URL=URL ; 
+    }
+    
+    public String getURL(){
+        return this.URL ; 
+    }
+    public void setTF(int TF){
+        this.TF=TF ; 
+    }
+    public int getTF(){
+        return this.TF ; 
+    }
+    
+    public void addOccurence(String tagName){
+        if(occurences.containsKey(tagName)){
+            Integer i = occurences.get(tagName); 
+            i++; 
+            occurences.replace(tagName, i); 
+        }
+        else
+            occurences.put(tagName,1); 
+    }
+    public Integer getTagOccurences(String tagName){
+        if(occurences.containsKey(tagName))
+            return occurences.get(tagName); 
+        else
+            return 0 ; 
+    }
+    public void setOccurence(String tagName, Integer numberOfOcc){
+        if(occurences.containsKey(tagName))
+            occurences.replace(tagName, numberOfOcc); 
+        else
+            occurences.put(tagName, numberOfOcc); 
+    }
+    
+    
+    
+  
+    
+}
